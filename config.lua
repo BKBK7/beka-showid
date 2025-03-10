1689eb354f23c9c6281ef047b66bf9f567df3bd4
beka <123653551+BKBK7@users.noreply.github.com>
2025-03-10 17:48:18 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
 Config = {}
 
+Config.Framework = 'VORP'   -- VORP | RSG
 Config.UseKey = true
 Config.Key = 0x446258B6 -- pageup
 Config.Distance = 15
-Config.Color = {247, 112, 22}
+Config.Color = {255, 255, 255}
 Config.Scale = 0.4
 Config.Textfont = 1 -- 0-5
-Config.Info = 'name'  -- 'name' | 'steam' | 'license' | false: only server ID
+Config.Info = 'name'  -- 'name' | 'playername' | 'steam' | 'license' | false: only server ID | 
